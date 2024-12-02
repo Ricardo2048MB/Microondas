cd66c2e167655902d58d62fedadbfea4d23e6c18
Ricardo Muñoz Bolaños <ricardoucxiu@gmail.com>
2024-12-02 00:13:57 -0600

    La mitad de arriba funciona y quizás no cumple con todos los requerimientos. Pero...

--- consola_de_comandos.py
+++ consola_de_comandos.py
@@ -4,6 +4,12 @@ import time
 class Microondas(cmd.Cmd):
     intro = 'Bienvenido al microondas. Escribe "help" para ver los comandos disponibles.'
     prompt = '(microondas) '
+    
+    presets = {
+        'palomitas': 3 * 60,  # 3 minutos
+        'yakimeshi': 2 * 60 + 30,  # 2 minutos y 30 segundos
+        'spaghetti': 1 * 60 + 45  # 1 minuto y 45 segundos
+    }
 
     def do_calentar(self, line):
         """
@@ -11,13 +17,15 @@ Calienta la comida por el tiempo especificado en segundos.
         
 Sintaxis
         
-calentar <segundos>
+calentar <segundos | preset>
+
+Presets disponibles: palomitas, yakimeshi, y spaghetti.
         
         """
-        try:
-            tiempo_segundos = int(line)
+        if line in self.presets:
+            tiempo_segundos = self.presets[line]
             minutos, segundos = divmod(tiempo_segundos, 60)
-            
+            alimento = line
             while minutos >= 0 and segundos >= 0:
                 print(f"{minutos:02d}:{segundos:02d}", end='\r')  # Imprime y regresa al inicio de la línea
                 time.sleep(1)  # Espera 1 segundo
@@ -25,9 +33,22 @@ calentar <segundos>
                 if segundos < 0:
                     minutos -= 1
                     segundos = 59
-            print("¡Comida lista!")
-        except ValueError:
-            print("Por favor, ingresa un número entero para el tiempo.")
+            print(f"¡{alimento.capitalize()} listo! Buen provecho.")
+        else:
+            try:
+                tiempo_segundos = int(line)
+                minutos, segundos = divmod(tiempo_segundos, 60)
+                alimento = input("¿Qué alimento estás calentando? ")
+                while minutos >= 0 and segundos >= 0:
+                    print(f"{minutos:02d}:{segundos:02d}", end='\r')  # Imprime y regresa al inicio de la línea
+                    time.sleep(1)  # Espera 1 segundo
+                    segundos -= 1
+                    if segundos < 0:
+                        minutos -= 1
+                        segundos = 59
+                print(f"¡{alimento.capitalize()} listo! Buen provecho.")
+            except ValueError:
+                print("Por favor, ingresa un número entero para el tiempo o el nombre de un preset.")
 
     def do_descongelar(self, line):
         """
